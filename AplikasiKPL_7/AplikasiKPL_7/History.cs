492c79b6d52d0acdea9ff05754cf77636fd9e457
arya-1302194093 <79740589+arya-1302194093@users.noreply.github.com>
2021-06-29 08:39:49 -0700

    Update Beberapa Fitur

--- AplikasiKPL_7/AplikasiKPL_7/History.cs
+++ AplikasiKPL_7/AplikasiKPL_7/History.cs
@@ -26,5 +26,106 @@ namespace AplikasiKPL_7
         {
             this.Hide(); Menu mn = new Menu(); mn.Show();
         }
+
+        private void Box1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Senin_Click(object sender, EventArgs e)
+        {
+            Box1.Text = "Main Gensin Impek";
+            Box2.Text = "Main Futsal";
+            Box3.Text = "Main PANJIE";
+            Box4.Text = "Main PEBEH";
+            ts1.Text = "Terselesaikan";
+            ts2.Text = "Terselesaikan";
+            ts3.Text = "Terselesaikan";
+            ts4.Text = "Terselesaikan";
+            jam1.Text = "12.00";
+            jam2.Text = "17.00";
+            jam3.Text = "18.00";
+            jam4.Text = "20.00";
+        }
+
+        private void Selasa_Click(object sender, EventArgs e)
+        {
+            Box1.Text = "Belajar Algoritma";
+            Box2.Text = "Pergi Pengajian";
+            Box3.Text = "Baca Al-Quran";
+            Box4.Text = "Belajar KPL";
+            ts1.Text = "Terselesaikan";
+            ts2.Text = "Terselesaikan";
+            ts3.Text = "Terselesaikan";
+            ts4.Text = "Tidak Terselesaikan";
+            jam1.Text = "12.00";
+            jam2.Text = "05.00";
+            jam3.Text = "16.45";
+            jam4.Text = "14.00";
+        }
+
+        private void rabu_Click(object sender, EventArgs e)
+        {
+            Box1.Text = "-------";
+            Box2.Text = "-------";
+            Box3.Text = "-------";
+            Box4.Text = "Pergi Pratikum";
+            ts1.Text = "-------";
+            ts2.Text = "-------";
+            ts3.Text = "-------";
+            ts4.Text = "Terselesaikan";
+            jam1.Text = "-------";
+            jam2.Text = "-------";
+            jam3.Text = "-------";
+            jam4.Text = "10.00";
+        }
+
+        private void Kamis_Click(object sender, EventArgs e)
+        {
+            Box1.Text = "-------";
+            Box2.Text = "-------";
+            Box3.Text = "Olahraga Tinju";
+            Box4.Text = "Belajar KPL";
+            ts1.Text = "-------";
+            ts2.Text = "-------";
+            ts3.Text = "Terselesaikan";
+            ts4.Text = "Terselesaikan";
+            jam1.Text = "-------";
+            jam2.Text = "-------";
+            jam3.Text = "14.00";
+            jam4.Text = "16.45";
+        }
+
+        private void Jumaat_Click(object sender, EventArgs e)
+        {
+            Box1.Text = "Bantu Orangtua";
+            Box2.Text = "Belajar Hadis";
+            Box3.Text = "Belajar Algoritma";
+            Box4.Text = "Belajar KPL";
+            ts1.Text = "Terselesaikan";
+            ts2.Text = "Terselesaikan";
+            ts3.Text = "Terselesaikan";
+            ts4.Text = "Terselesaikan";
+            jam1.Text = "10.00";
+            jam2.Text = "12.00";
+            jam3.Text = "14.00";
+            jam4.Text = "16.45";
+        }
+
+        private void Sabtu_Click(object sender, EventArgs e)
+        {
+            Box1.Text = "Bantu Orangtua";
+            Box2.Text = "Belajar Agama";
+            Box3.Text = "Belajar Golang";
+            Box4.Text = "Tournamen Pebeh";
+            ts1.Text = "Terselesaikan";
+            ts2.Text = "Terselesaikan";
+            ts3.Text = "Terselesaikan";
+            ts4.Text = "Terselesaikan";
+            jam1.Text = "12.00";
+            jam2.Text = "13.00";
+            jam3.Text = "16.00";
+            jam4.Text = "18.25";
+        }
     }
 }
